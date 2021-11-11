6f524b5b3a333127f0c61e11aade29d4f255fb2e
ZhukDima <zhuk@gmail.com>
2021-11-11 16:58:39 +0200

    Scripted auto-commit on change (2021-11-11 16:58:39) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Objects;
 
@@ -9,11 +10,13 @@ public class App {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flights = sc.textFile("L_AIRPORT_ID.csv");
-        String firstFlight = flights.first();
-        flights = flights.filter(e -> firstFlight != e);
-        flights.mapToPair(flight -> {
-            Flight.parseCSV(flight);
+        JavaRDD<String> dataFlights = sc.textFile("L_AIRPORT_ID.csv");
+        String firstFlight = dataFlights.first();
+        dataFlights = dataFlights.filter(e -> firstFlight != e);
+        dataFlights.mapToPair(dataFlight -> {
+            Flight flight = Flight.parseCSV(dataFlight);
+            return new Tuple2<>(
+                    new Tuple2<>(flight.getOriginAirportId(), flight.getDestAirportId()), flight);
         });
 
         JavaRDD<String> airports = sc.textFile("664600583_T_ONTIME_sample.csv");
