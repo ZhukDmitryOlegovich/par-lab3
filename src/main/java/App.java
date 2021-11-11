963975bcc8c81b7dc9621677e0cfad72b58efa54
ZhukDima <zhuk@gmail.com>
2021-11-11 16:23:54 +0200

    Scripted auto-commit on change (2021-11-11 16:23:54) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("lab5");
+        SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
