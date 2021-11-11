ab1236804334357c2c2867254d7e7cde0175393d
ZhukDima <zhuk@gmail.com>
2021-11-11 16:47:29 +0200

    Scripted auto-commit on change (2021-11-11 16:47:28) by gitwatch.sh

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,6 +1,5 @@
 public class Flight {
-    private final int code;
-    private final float delay;
+    private final boolean cancelled;
 
     private static final int DEST_AIRPORT_ID_CSV_INDEX = 14;
     private static final int CANCELLED_CSV_INDEX = 19;
@@ -9,9 +8,8 @@ public class Flight {
     private static final String CSV_REPLACEMENT = "";
     private static final String CSV_SPLIT_REGEX = ",";
 
-    public Flight(int code, float delay) {
-        this.code = code;
-        this.delay = delay;
+    public Flight(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 
     public float getDelay() {
@@ -25,7 +23,7 @@ public class Flight {
     public static Flight parseCSV(String csv) {
         String[] list = csv.replaceAll(CSV_REPLACE_REGEX, CSV_REPLACEMENT).split(CSV_SPLIT_REGEX);
         return new Flight(
-                Integer.parseInt(list[DEST_AIRPORT_ID_CSV_INDEX]),
+                list[CANCELLED_CSV_INDEX].isEmpty(),
                 list[DELAY_CSV_INDEX].length() > 0 ? Float.parseFloat(list[DELAY_CSV_INDEX]) : 0
         );
     }
