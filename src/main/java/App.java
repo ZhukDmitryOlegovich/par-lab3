320d69a11b2899284fbbd8f3bdae8deb6e3bbf45
ZhukDima <zhuk@gmail.com>
2021-11-11 20:17:55 +0200

    Scripted auto-commit on change (2021-11-11 20:17:55) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,37 +9,45 @@ import java.util.Map;
 import java.util.Objects;
 
 public class App {
+    private static final String APP_NAME = "lab3";
+    private static final String INPUT_AIRPORT_CSV_PATH = "L_AIRPORT_ID.csv";
+    private static final String INPUT_FLIGHT_CSV_PATH = "664600583_T_ONTIME_sample.csv";
+    private static final String FORMAT = "from:%80s,to:%80s";
+    private static final String OUTPUT_PATH = "output";
+
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("lab3");
+        SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> dataAirports = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> dataAirports = sc.textFile(INPUT_AIRPORT_CSV_PATH);
         String firstDataAirport = dataAirports.first();
-        JavaPairRDD<Integer, String> airportsRDD = dataAirports
-                .filter(dataAirport -> !Objects.equals(firstDataAirport, dataAirport))
-                .mapToPair(dataAirport -> Airport.parseCSV(dataAirport).getTuple());
 
-        final Broadcast<Map<Integer, String>> airportsBroadcaster = sc.broadcast(airportsRDD.collectAsMap());
-
-        JavaRDD<String> dataFlights = sc.textFile("664600583_T_ONTIME_sample.csv");
-        String firstDataFlight = dataFlights.first();
-        JavaPairRDD<Tuple2<Integer, Integer>, Flight> airportFlights = dataFlights
-                .filter(dataFlight -> !Objects.equals(firstDataFlight, dataFlight))
-                .mapToPair(dataFlight -> Flight.parseCSV(dataFlight).getTupleWithAirports());
-
-        JavaPairRDD<Tuple2<Integer, Integer>, FlightReduce> flightStatistics = airportFlights.combineByKey(
-                FlightReduce::new,
-                FlightReduce::accumulate,
-                FlightReduce::merge
+        final Broadcast<Map<Integer, String>> airportsBroadcaster = sc.broadcast(
+                dataAirports
+                        .filter(dataAirport -> !Objects.equals(firstDataAirport, dataAirport))
+                        .mapToPair(dataAirport -> Airport.parseCSV(dataAirport).getTuple())
+                        .collectAsMap()
         );
 
-        flightStatistics.mapToPair(statistics -> new Tuple2<>(
-                statistics._2.getStatistics(),
-                String.format(
-                        "from:%80s,to:%80s",
-                        airportsBroadcaster.value().get(statistics._1._1),
-                        airportsBroadcaster.value().get(statistics._1._2)
+        JavaRDD<String> dataFlights = sc.textFile(INPUT_FLIGHT_CSV_PATH);
+        String firstDataFlight = dataFlights.first();
+
+        dataFlights
+                .filter(dataFlight -> !Objects.equals(firstDataFlight, dataFlight))
+                .mapToPair(dataFlight -> Flight.parseCSV(dataFlight).getTupleWithAirports())
+                .combineByKey(
+                        FlightReduce::new,
+                        FlightReduce::accumulate,
+                        FlightReduce::merge
                 )
-        )).saveAsTextFile("output");
+                .mapToPair(statistics -> new Tuple2<>(
+                        statistics._2.getStatistics(),
+                        String.format(
+                                FORMAT,
+                                airportsBroadcaster.value().get(statistics._1._1),
+                                airportsBroadcaster.value().get(statistics._1._2)
+                        )
+                ))
+                .saveAsTextFile(OUTPUT_PATH);
     }
 }
