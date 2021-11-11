dfce3a989ae99a371dafdfbc7a6d711771e0db22
ZhukDima <zhuk@gmail.com>
2021-11-11 19:36:07 +0200

    Scripted auto-commit on change (2021-11-11 19:36:07) by gitwatch.sh

--- src/main/java/FlightReduce.java
+++ src/main/java/FlightReduce.java
@@ -6,7 +6,7 @@ public class FlightReduce implements Serializable {
     private int countCancelled;
     private int countFlights;
 
-    private static final String FORMAT = "max:%10.5f, \tcancelled (%%): %-10.5f, \tdelay (%%): %-10.5f";
+    private static final String FORMAT = "max:%10.2f, \tcancelled (%%): %-10.5f, \tdelay (%%): %-10.5f";
 
     public FlightReduce(Flight flight) {
         float delay = flight.getDelayTime();
