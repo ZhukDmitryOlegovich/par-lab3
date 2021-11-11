b9be7cb478409179565621c68a7e6f827e732429
ZhukDima <zhuk@gmail.com>
2021-11-11 18:04:45 +0200

    Scripted auto-commit on change (2021-11-11 18:04:45) by gitwatch.sh

--- src/main/java/FlightReduce.java
+++ src/main/java/FlightReduce.java
@@ -1,2 +1,14 @@
-public class FlightReduce {
+import scala.Serializable;
+
+public class FlightReduce implements Serializable {
+    private float maxDelay;
+    private int countDelays;
+    private int countCancelled;
+    private int countFlights;
+
+    public FlightReduce(float delay, boolean canceled) {
+        maxDelay = delay;
+        countDelays = delay == 0 ? 0 : 1;
+        countCancelled = Integer(canceled);
+    }
 }
